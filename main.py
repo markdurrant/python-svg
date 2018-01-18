10d862ac1313ed8193ff1802f7a9bffd51500f58
Mark Durrant <mark.j.durrant@gmail.com>
2018-01-18 21:55:44 +0000

    do the dash

--- main.py
+++ main.py
@@ -9,49 +9,47 @@ from point import Point
 
 
 fuscia = Pen(weight = 0.5, color = "#f09")
-cyan = Pen(weight = 0.25, color = "#09f")
-black = Pen(weight = 1)
+cyan = Pen(weight = 0.75, color = "#09f")
+black = Pen(weight = 0.25)
 
-paper = Paper(pens = [black], width = 105, height = 73)
+paper = Paper(pens = [cyan, black], width = 105, height = 73)
 
 def drawGuide(p1, p2): 
   return Path(points = [p1, p2]).setPen(fuscia)
 
+def drawDash(p, length):
+  dash = Path(points = [
+    Point(p.x, p.y + length / 2),
+    Point(p.x, p.y - length / 2)
+  ]).rotate(random() * 360)
+  
+  return dash
 
 squareSize = 34
 square = Path(points = [
-  Point(paper.center.x + squareSize / 2, paper.center.y + squareSize / 2 + 3),
-  Point(paper.center.x + squareSize / 2, paper.center.y - squareSize / 2 + 3),
-  Point(paper.center.x - squareSize / 2, paper.center.y - squareSize / 2 + 3),
-  Point(paper.center.x - squareSize / 2, paper.center.y + squareSize / 2 + 3)
-], closed = True).setPen(cyan)
+  Point(paper.center.x + squareSize / 2, paper.center.y + squareSize / 2),
+  Point(paper.center.x + squareSize / 2, paper.center.y - squareSize / 2),
+  Point(paper.center.x - squareSize / 2, paper.center.y - squareSize / 2),
+  Point(paper.center.x - squareSize / 2, paper.center.y + squareSize / 2)
+], closed = True)
 
-pointA = Point(square.topLeft.x, square.topLeft.y + uniform(2, 12))
-pointB = square.bottomLeft.clone()
-pointC = square.bottomRight.clone()
-pointD = Point(square.topRight.x, square.topRight.y + uniform(2, 12))
+spotlight = Point(
+  paper.center.x + random() * squareSize - squareSize / 2,
+  paper.center.y + random() * squareSize - squareSize / 2
+)
 
-pointE = Point(square.right + squareSize * random(), square.topCenter.y - uniform(2, 8))
-pointF = Point(square.right + squareSize * random(), square.bottom)
+for i in range(1, 1200):
+  dashPoint = Point(
+    paper.center.x + random() * squareSize - squareSize / 2,
+    paper.center.y + random() * squareSize - squareSize / 2
+  )
 
+  if i % 2 == 0:
+    dashPen = black
+  else:
+    dashPen = black 
 
-guideX = drawGuide(pointA, pointB)
-guideY = drawGuide(pointE, pointF)
-guideZ = drawGuide(pointD, pointC)
+  drawDash(dashPoint, 2.5 - abs(dashPoint.distanceTo(spotlight)) / 8).setPen(dashPen)
 
-numLines = 54
-numLines = numLines - 1
-
-for l in range(1, numLines):
-  offset = l * (square.bottom - square.top) / numLines
-
-  Path(points = [
-    guideX.pointAtDistance(guideX.length() / numLines * l),
-    guideY.pointAtDistance(guideY.length() / numLines * l),
-    guideZ.pointAtDistance(guideZ.length() / numLines * l),
-  ]).setPen(black)
-
-Path(points = [pointA, pointB, pointF, pointC, pointD, pointE], closed = True).setPen(black)
-
-black.saveGcode("test.nc")
+# black.saveGcode("test.nc")
 paper.save("test.svg")
